b80981b90b9ed6ca525b2a7b67663f3acd561d96
KonPay08 <ykh.821.family@gmail.com>
2022-11-25 18:04:15 +0900

    useRef-basic

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -11,12 +11,20 @@ export const Counter: React.FC<{}> = () => {
     renderTimes.current += 1;
   },[value]);
 
+  const ref = useRef<HTMLInputElement>(null!);
+
+  const focusInput = () => {
+    ref.current.focus();
+  }
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment} >+1</button>
       <button onClick={decrement}>-1</button>
       <div>This component was re-renderd {renderTimes.current}</div>
+      <input ref={ref} type="text"></input>
+      <button onClick={focusInput} >Click Me</button>
     </div>
   );
 };
